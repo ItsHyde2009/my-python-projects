1630c5a845a9f3451bd67d0241bf51e69e7cb6d1
Álvaro Ibáñez Coedo <98840489+AlvaroIC@users.noreply.github.com>
2024-02-07 12:08:48 +0100

    Fixes and recommendations for the SimplePyCalculator (part 1)
    Hey there! 
    
    I will perform some other changes when I have time for it, but for now, here are some corrections and tips. As a summary:
    
    -You don't really need to convert to float each variable when you perform an operation, you can convert them when asking for them as input and store them as your desired type so you can freely work with them later on.
    
    -Remember, you can use functions for repetitive tasks, such as asking for the numbers as input. This way, you avoid repeating code or having to ask for the number before asking for the operation to perform, thus avoiding having to ask the user to omit the second number in case he wants to perform a square root operation.
    
    -New Python versions implement "match", a switch-like statement that can be useful for cases like this one. Please, check: https://docs.python.org/es/dev/reference/compound_stmts.html#match
    
    - The Python  operator for powers is "**", not "^". This is a very common mistake, so not a big deal.
    
    Well done!

--- Projects/simplePyCalculator.py
+++ Projects/simplePyCalculator.py
@@ -1,20 +1,30 @@
 import math
-n1 = input("Insert a number: ")
-n2 = input("Insert another number (omit for square root): ")
-op = input("Please select the operation: ")
-if op == "+":
-#I know that the next part is non-optimised as heck, but for now I can't figure out how to make it better.
-#And yes, I don't know how to convert floats into ints to make the final result an int when there are no decimal numbers.
-    print("The result of the operation is: ", float(n1) + float(n2))
-elif op == "-":
-    print("The result of the operation is: ", float(n1) - float(n2))
-elif op == "*":
-    print("The result of the operation is: ", float(n1) * float(n2))
-elif op == "/":
-    print("The result of the operation is: ", float(n1) / float(n2))
-elif op == "^":
-    print("The result of the operation is: ", float(n1) ^ float(n2))
-elif op == "v":
-    print("The result of the operation is: ", math.sqrt(float(n1)))
-else:
-    print("The operator you entered is invalid for this calculator app")
+
+def ask_for_input():
+    n1 = float(input("Insert the first number: "));
+    n2 = float(input("Insert the second number: "));
+    return n1, n2;
+
+op = input("Please, select your operation: ");
+
+match op:
+    case "+":
+        numbers = ask_for_input();
+        print("The result of the operation is: ", numbers[0] + numbers[1]);
+    case "-":
+        numbers = ask_for_input();
+        print("The result of the operation is: ", numbers[0] - numbers[1]);
+    case "*":
+        numbers = ask_for_input();
+        print("The result of the operation is: ", numbers[0] * numbers[1]);
+    case "/":
+        numbers = ask_for_input();
+        print("The result of the operation is: ", numbers[0] / numbers[1]);
+    case "^":
+        numbers = ask_for_input();
+        print("The result of the operation is: ", numbers[0] ** numbers[1]);
+    case "v":
+        print("The result of the operation is: ", math.sqrt(float(input("Insert the number: "))));
+    case _:
+        print("The operator you entered is invalid for this calculator app!");
+
