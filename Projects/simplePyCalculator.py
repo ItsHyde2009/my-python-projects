20aec1d49e29a6d920a21e285900a5cf1388fc85
Álvaro Ibáñez Coedo <98840489+AlvaroIC@users.noreply.github.com>
2024-02-08 15:31:54 +0100

    Fixes and recommendations for the SimplePyCalculator (part 2)
    Hi again!
    
    As I told you in the previous commit message, I left a few suggestions and changes for this ocasion.
    
    One of the most important principles of good code is robustness. As this concept is of utmost importance for every developer, I encourage you to check out this short explanation offered by Wikipedia: https://en.wikipedia.org/wiki/Robustness_(computer_science)#:~:text=Robust%20programming%20is%20a%20style,more%20easily%20debug%20the%20program.
    
    Once you have checked it, you will understand how it is related to the changes I have performed over your code. Now, the user does not have to restart the calculator every time he wants to perform an operation or every time he inputs an incorrect character. Now, he will be asked for input until he explicitly wants to exit the program, making it less prone to fail.
    
    I hope you find this useful and will make you improve your code overtime.

--- Projects/simplePyCalculator.py
+++ Projects/simplePyCalculator.py
@@ -5,26 +5,47 @@ def ask_for_input():
     n2 = float(input("Insert the second number: "));
     return n1, n2;
 
+print("Select the operation you want to perform:\n\t" 
+     + "+) Addition\n\t" 
+     + "-) Substraction\n\t" 
+     + "*) Multiplication\n\t"  
+     + "/) Division\n\t"
+     + "^) Power\n\t" 
+     + "v) Square root\n"
+ + 'Type "exit" to stop the execution of the calculator\n\n')
+
 op = input("Please, select your operation: ");
 
-match op:
-    case "+":
-        numbers = ask_for_input();
-        print("The result of the operation is: ", numbers[0] + numbers[1]);
-    case "-":
-        numbers = ask_for_input();
-        print("The result of the operation is: ", numbers[0] - numbers[1]);
-    case "*":
-        numbers = ask_for_input();
-        print("The result of the operation is: ", numbers[0] * numbers[1]);
-    case "/":
-        numbers = ask_for_input();
-        print("The result of the operation is: ", numbers[0] / numbers[1]);
-    case "^":
-        numbers = ask_for_input();
-        print("The result of the operation is: ", numbers[0] ** numbers[1]);
-    case "v":
-        print("The result of the operation is: ", math.sqrt(float(input("Insert the number: "))));
-    case _:
-        print("The operator you entered is invalid for this calculator app!");
+while(op != "exit"):
+    match op:
+        case "+":
+            numbers = ask_for_input();
+            print("The result of the operation is: ", numbers[0] + numbers[1]);
+        case "-":
+            numbers = ask_for_input();
+            print("The result of the operation is: ", numbers[0] - numbers[1]);
+        case "*":
+            numbers = ask_for_input();
+            print("The result of the operation is: ", numbers[0] * numbers[1]);
+        case "/":
+            numbers = ask_for_input();
+            print("The result of the operation is: ", numbers[0] / numbers[1]);
+        case "^":
+            numbers = ask_for_input();
+            print("The result of the operation is: ", numbers[0] ** numbers[1]);
+        case "v":
+            print("The result of the operation is: ", math.sqrt(float(input("Insert the number: "))));
+        case _:
+            print("The operator you entered is invalid for this calculator app!");
+     
+    print("\n\nSelect the operation you want to perform:\n\t" 
+        + "+) Addition\n\t" 
+        + "-) Substraction\n\t" 
+        + "*) Multiplication\n\t"  
+        + "/) Division\n\t"
+        + "^) Power\n\t" 
+        + "v) Square root\n"
+    + 'Type "exit" to stop the execution of the calculator\n\n')
+    
+    op = input("Please, select your operation: ");
 
